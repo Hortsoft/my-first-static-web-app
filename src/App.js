70f857ab17c1e04a03230098855b4d79a9324946
Hortsoft <71443445+Hortsoft@users.noreply.github.com>
2021-08-18 14:34:23 +1200

    fff

--- src/App.js
+++ src/App.js
@@ -2,31 +2,17 @@ import React, { useState, useEffect } from 'react';
 
 function App() {
   const [data, setData] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState();
-
+  
   useEffect(() => {
     (async function () {
       const { text } = await( await fetch(`/api/message`)).json();
       setData(text);
     })
-    
     .catch((err) => {
-      setError(err);
-    })
-
-    .finally(() => {
-      setLoading(false);
+      console.log(err);
     });
-  }, []);
+}, []);
 
-  if (loading) {
-    return <p>You link data is loading...</p>;
-  }
-
-  if (error || !Array.isArray(data)) {
-    return <p>There was an error loading your data!</p>;
-  }
   return (
     <div> 
     <ul>
