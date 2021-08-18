c215cae4b0769c2938a320ea799ab80b36c59488
Hortsoft <71443445+Hortsoft@users.noreply.github.com>
2021-08-18 16:28:25 +1200

    new methos

--- src/App.js
+++ src/App.js
@@ -1,28 +1,59 @@
-import React, { useState, useEffect, Suspense } from 'react';
+import React from 'react';
+import { useAsync } from 'react-async';
 
+
+// Then we'll fetch user data from this API
+const loadUsers = async () =>
+  await fetch(`/api/message`)
+    .then(res => (res.ok ? res : Promise.reject(res)))
+    .then(res => res.json())
+ 
 function App() {
-  const [data, setData] = useState([]);
+  const { data, error, isLoading } = useAsync({ promiseFn: loadUsers })
+  if (isLoading) return "Loading..."
+  if (error) return `Something went wrong: ${error.message}`
+  if (data)
   
-  useEffect(() => {
-    (async function () {
-      const { text } = await( await fetch(`/api/message`)).json();
-      setData(text);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-}, []);
-
+  // The rendered component
   return (
-    <div>
-      <Suspense fallback={<div>Loading...</div>}></Suspense>
-      <ul>
-        {data.map((item) => (
-          <li key={item}>{item.name}</li>
-        ))}
-      </ul>
+    <div className="container">
+      <div>
+        <h2>React Async - Cosomos Trees</h2>
+      </div>
+      {data.map(user=> (
+        <div key={user.id} className="row">
+          <div className="col-md-12">
+            <p>{user.name}</p>
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
-
 export default App;
+
+   
+//function App() {
+//  const [data, setData] = useState([]);
+  
+//  useEffect(() => {
+  //  (async function () {
+//      const { text } = await( await fetch(`/api/message`)).json();
+  //    setData(text);
+//    })
+//    .catch((err) => {
+//      console.log(err);
+ //   });
+//}, []);
+
+//  return (
+//    <div>
+//      <Suspense fallback={<div>Loading...</div>}></Suspense>
+//      <ul>
+//        {data.map((item) => (
+//          <li key={item}>{item.name}</li>
+//        ))}
+//      </ul>
+//    </div>
+//  );
+//}
