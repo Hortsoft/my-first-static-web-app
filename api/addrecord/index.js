c26f2ad4badfe5efd6ed26fb3c3ffc79dff4a58c
Hortsoft <71443445+Hortsoft@users.noreply.github.com>
2021-08-19 17:04:26 +1200

    ddd

--- api/addrecord/index.js
+++ api/addrecord/index.js
@@ -4,17 +4,9 @@ module.exports = async function (context, req) {
         id: "5",
         name: "Pohutukawa"
       };
-
-    if (newItem != null) {
-
-        context.bindings.outputDocument = newItem;
+       
+      context.bindings.outputDocument = newItem;
         context.done();
-
-    }else{
-        context.res = {
-            status: 400,
-            body: {'result': 'error'}
-        };
-    }
+ 
 };
 
