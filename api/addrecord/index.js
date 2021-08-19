a8e58f253c6cdc82adce91eb9c99c78bb4ab7878
Hortsoft <71443445+Hortsoft@users.noreply.github.com>
2021-08-20 10:39:06 +1200

    ll

--- api/addrecord/index.js
+++ api/addrecord/index.js
@@ -1,9 +1,16 @@
 module.exports = function (context, req) {
-  const newItem = {
-    id: "5",
-    name: "Pohutukawa",
-  };
+  
+   // const newItem = {
+    //id: "5",
+    //name: "Pohutukawa",
+  //};
 
-  context.bindings.outputDocument = JSON.stringify(newItem);
+  //context.bindings.outputDocument = JSON.stringify(newItem);
+  context.bindings.outputDocument = JSON.stringify({
+    id: context.bindings.myQueueItem.id,
+    name: context.bindings.myQueueItem.name
+  });
+
   context.done();
+  
 };
