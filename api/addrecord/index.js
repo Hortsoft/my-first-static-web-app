55bab80c18dc3a0e9afb79bfc2cb72f0eb3b92a1
Hortsoft <71443445+Hortsoft@users.noreply.github.com>
2021-08-20 11:43:32 +1200

    t

--- api/addrecord/index.js
+++ api/addrecord/index.js
@@ -1,37 +1,34 @@
 module.exports = async function (context, req) {
+  req.body.id = "4";
+  req.body.name = "Putaputaweta";
 
-    const id = "4"; 
-    const name = "Putaputaweta"; 
+  const name = req.body.name;
 
-    if (id && name != null) {
-
-        context.bindings.outputDocument = JSON.stringify({
-                 id: "5",
-                 name: "Pohutukawa"
-        }); 
-        context.res = {
-            body: {'result': 'success'}
-        };
-    }else{
-        context.res = {
-            status: 400,
-            body: {'result': 'error'}
-        };
-    }
+  if (name != null) {
+    context.bindings.outputDocument = req.body;
+    context.res = {
+      body: { result: "success" },
+    };
+  } else {
+    context.res = {
+      status: 400,
+      body: { result: "error" },
+    };
+  }
 };
 //module.exports = function (context, req) {
-  
-   // const newItem = {
-    //id: "5",
-    //name: "Pohutukawa",
-  //};
 
-  //context.bindings.outputDocument = JSON.stringify(newItem);
- // context.bindings.outputDocument = JSON.stringify({
-  //    id: "5",
-  //    name: "Pohutukawa"
-  //});
+// const newItem = {
+//id: "5",
+//name: "Pohutukawa",
+//};
 
-  //context.done();
+//context.bindings.outputDocument = JSON.stringify(newItem);
+// context.bindings.outputDocument = JSON.stringify({
+//    id: "5",
+//    name: "Pohutukawa"
+//});
+
+//context.done();
 
 //};
