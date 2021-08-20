d166eccf1c5ba4a67d7aa56fee7449d9a2e9f532
Hortsoft <71443445+Hortsoft@users.noreply.github.com>
2021-08-20 13:01:51 +1200

    hh

--- api/addrecord/index.js
+++ api/addrecord/index.js
@@ -1,36 +1,14 @@
 module.exports = function (context, req) {
+  const newItem = {
+    id: "5",
+    name: "Pohutukawa",
+  };
 
-    const newItem = {
-        id: "5",
-        name: "Pohutukawa",
-        };
- 
-  if (newItem != null) { 
-    context.bindings.outputDocument = newItem;
-    context.res = {
-        status: 200,
-        body: { result: "success" },
-    };
-  } else {
-    context.res = {
-      status: 400,
-      body: { result: "error" },
-    };
-  }
+  context.bindings.outputDocument = JSON.stringify(newItem);
+  // context.bindings.outputDocument = JSON.stringify({
+  //    id: "5",
+  //    name: "Pohutukawa"
+  //});
+
+  context.done();
 };
-//module.exports = function (context, req) {
-
-// const newItem = {
-//id: "5",
-//name: "Pohutukawa",
-//};
-
-//context.bindings.outputDocument = JSON.stringify(newItem);
-// context.bindings.outputDocument = JSON.stringify({
-//    id: "5",
-//    name: "Pohutukawa"
-//});
-
-//context.done();
-
-//};
